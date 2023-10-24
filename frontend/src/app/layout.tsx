c9dc5d49a5a91e6a1c4b6606e620855a5b4fbb24
Alex Sobral <alex.dacostasobral@gmail.com>
2023-10-24 11:19:34 -0300

    first commit

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Poppins } from 'next/font/google'
+import './globals.css'
+
+const poppins = Poppins({ subsets: ['latin'], weight: ['200', '400', '500', '700'] })
+
+export const metadata: Metadata = {
+  title: 'E-commerce',
+  description: 'My first E-commerce',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>{children}</body>
+    </html>
+  )
+}
